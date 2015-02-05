7c1b1ed2ae873b6a7c75daf2d5d22051ab437c7d
x86 <ya.freelan@yandex.ru>
2015-02-05 21:14:56 +0300

    format SampleAi/Program.cs

--- SampleAi/Program.cs
+++ SampleAi/Program.cs
@@ -6,30 +6,30 @@ using System.Threading.Tasks;
 
 namespace SampleAi
 {
-	class Program
-	{
-		// Управляющая программа battleships.exe будет запускать этот файл и перенаправлять стандартные потоки ввода и вывода.
-		//
-		// Вам нужно читать информацию с консоли и писать команды на консоль.
-		// Конец ввода — это сигнал к завершению программы.
+    class Program
+    {
+        // Управляющая программа battleships.exe будет запускать этот файл и перенаправлять стандартные потоки ввода и вывода.
+        //
+        // Вам нужно читать информацию с консоли и писать команды на консоль.
+        // Конец ввода — это сигнал к завершению программы.
 
-		static void Main()
-		{
-			var r = new Random();
-			while (true)
-			{
-				var line = Console.ReadLine();
-				if (line == null) return;
-				// line имеет один из следующих форматов:
-				// Init <map_width> <map_height> <ship1_size> <ship2_size> ...
-				// Wound <last_shoot_X> <last_shoot_Y>
-				// Kill <last_shoot_X> <last_shoot_Y>
-				// Miss <last_shoot_X> <last_shoot_Y>
+        static void Main( )
+        {
+            var r = new Random();
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == null) return;
+                // line имеет один из следующих форматов:
+                // Init <map_width> <map_height> <ship1_size> <ship2_size> ...
+                // Wound <last_shoot_X> <last_shoot_Y>
+                // Kill <last_shoot_X> <last_shoot_Y>
+                // Miss <last_shoot_X> <last_shoot_Y>
 
-				// Один экземпляр вашей программы может быть использван для проведения нескольких игр подряд.
-				// Сообщение Init сигнализирует о том, что началась новая игра.
-				Console.WriteLine("{0} {1}", r.Next(20), r.Next(20));
-			}
-		}
-	}
+                // Один экземпляр вашей программы может быть использван для проведения нескольких игр подряд.
+                // Сообщение Init сигнализирует о том, что началась новая игра.
+                Console.WriteLine("{0} {1}", r.Next(20), r.Next(20));
+            }
+        }
+    }
 }
